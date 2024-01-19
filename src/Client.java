e65bc437dcc1064389963b387a535ccfedd3a17d
rae <raej0000@gmail.com>
2024-01-18 17:34:06 -0800

    fix desync

--- src/Client.java
+++ src/Client.java
@@ -1,46 +1,48 @@
+import java.io.Closeable;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.util.Scanner;
 
-public class Client {
+public class Client implements Closeable {
     static Networking myNetCon;
     static Scanner scanner = new Scanner(System.in);
     static String testMessage = "Hello World!";
 
     public static void main(String[] args) {
         myNetCon = new Networking();
+        System.out.print("Enter host ip/address: ");
+        String host = scanner.nextLine();
+        System.out.print("Enter server port: ");
+        int port = Integer.parseInt(scanner.nextLine());
         try {
-            myNetCon.startConnection("127.0.0.1",6666);
+            myNetCon.startConnection(host,port);
+            sendToServer("connection established");
         } catch (IOException e) {
             System.out.println("got error: " + e.getMessage());
+            System.exit(0);
         }
         System.out.print("Send a message: ");
         String userInput = scanner.nextLine();
         while (true) {
-            sendToServer(userInput);
             if (userInput.contains("stop")) {
-                System.out.println("send stop to server and stopping client...");
-                try{
-                    myNetCon.close();
-                }catch (IOException e) {
-                    System.out.println("got error while closing client: " + e.getMessage());
-                }
-                break;
+                System.out.println("stopping client...");
+                System.exit(0);
             }
+            sendToServer(userInput);
             System.out.print("Send a message: ");
             userInput = scanner.nextLine();
         }
     }
     public static void sendToServer(String msg) {
         try {
-            String resp = myNetCon.sendMsg(msg);
-            if (resp == null) {
-                System.out.println("no server response");
-            } else {
-                System.out.println("got response: " + resp);
-            }
+            myNetCon.sendMsg(msg);
         } catch (Exception e) {
             System.out.println("got exception: " + e.getMessage());
         }
+    }
+
+    @Override
+    public void close() throws IOException {
+        myNetCon.close();
     }
 }
