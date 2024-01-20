ec455c020a248fbca9a8bd092e201c1b4dbd1ec2
rae <raej0000@gmail.com>
2024-01-19 20:43:37 -0800

    no more consoles

--- src/Client.java
+++ src/Client.java
@@ -1,7 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.Closeable;
 import java.io.IOException;
 import java.util.Scanner;
@@ -10,24 +9,24 @@ public class Client implements Closeable {
     static Networking myNetCon;
     static Scanner scanner = new Scanner(System.in);
     static JFrame frame;
-    static JTextField textField;
-    static JLabel label;
+    static JTextField msgField;
+    static JLabel msgLabel;
+    static int port;
+    static JLabel portLabel;
+    static JTextField portSelectField;
+    static String hostname;
+    static JLabel hostLabel;
+    static JTextField hostField;
+    static JButton connectButton;
     static JTextArea textArea;
     static JScrollPane scrollableTextArea;
+    static JPanel chatPane;
+    static JPanel configPane;
+
+    static boolean connected = false;
 
     public static void main(String[] args) {
         myNetCon = new Networking();
-        System.out.print("Enter host ip/address: ");
-        String host = scanner.nextLine();
-        System.out.print("Enter server port: ");
-        int port = Integer.parseInt(scanner.nextLine());
-        try {
-            myNetCon.startConnection(host,port);
-            sendToServer("connection established");
-        } catch (IOException e) {
-            System.out.println("got error: " + e.getMessage());
-            System.exit(0);
-        }
 
         windowInit();
     }
@@ -37,13 +36,11 @@ public class Client implements Closeable {
         frame.setMinimumSize(new Dimension(600,465));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        // begin messaging panel
         textArea = new JTextArea("",25,60);
-        textArea.append("ChatClient window open...");
         textArea.setSize(500,230);
         textArea.setLineWrap(true);
         textArea.setEditable(false);
-        textArea.setForeground(Color.white);
-        textArea.setBackground(Color.gray);
         textArea.setVisible(true);
 
         scrollableTextArea = new ModernScrollPane(textArea);
@@ -51,40 +48,77 @@ public class Client implements Closeable {
         scrollableTextArea.setSize(500,420);
         new SmartScroller(scrollableTextArea);
 
-        label = new JLabel("Send a message:");
-        label.setHorizontalAlignment(JLabel.LEFT);
-        label.setForeground(Color.white);
+        msgLabel = new JLabel("Send a message:");
+        msgLabel.setHorizontalAlignment(JLabel.LEFT);
 
-        textField = new JTextField(60);
-        textField.setForeground(Color.white);
-        textField.setBackground(Color.gray);
-        textField.setCaretColor(Color.white);
-        textField.setVisible(true);
+        msgField = new JTextField(60);
+        msgField.setCaretColor(Color.white);
+        msgField.setVisible(true);
 
-        Container contentPane = frame.getContentPane();
-        JPanel chatPane = new JPanel();
+        chatPane = new JPanel();
         chatPane.setLayout(new BoxLayout(chatPane,BoxLayout.Y_AXIS));
         chatPane.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
         chatPane.setBackground(Color.darkGray);
 
         chatPane.add(scrollableTextArea);
         chatPane.add(Box.createRigidArea(new Dimension(0,5)));
-        chatPane.add(label);
-        chatPane.add(textField);
+        chatPane.add(msgLabel);
+        chatPane.add(msgField);
 
+        for (Component comp : chatPane.getComponents()) {
+            comp.setBackground(Color.gray);
+            comp.setForeground(Color.white);
+        }
+
+        // begin config panel
+        portLabel = new JLabel("Current port: " + port);
+
+        portSelectField = new JTextField(5);
+        portSelectField.setSize(50,1);
+        portSelectField.setCaretColor(Color.white);
+
+        hostLabel = new JLabel("Current host: " + hostname);
+
+        hostField = new JTextField(25);
+        hostField.setCaretColor(Color.white);
+
+        connectButton = new JButton("Connect");
+
+        configPane = new JPanel();
+        configPane.setLayout(new BoxLayout(configPane,BoxLayout.X_AXIS));
+        configPane.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+        configPane.setBackground(Color.darkGray);
+
+        configPane.add(portLabel);
+        configPane.add(Box.createRigidArea(new Dimension(5,0)));
+        configPane.add(portSelectField);
+        configPane.add(Box.createRigidArea(new Dimension(5,0)));
+        configPane.add(hostLabel);
+        configPane.add(Box.createRigidArea(new Dimension(5,0)));
+        configPane.add(hostField);
+        configPane.add(Box.createRigidArea(new Dimension(5,0)));
+        configPane.add(connectButton);
+
+        for (Component comp : configPane.getComponents()) {
+            comp.setBackground(Color.gray);
+            comp.setForeground(Color.white);
+        }
+
+        Container contentPane = frame.getContentPane();
         contentPane.add(chatPane, BorderLayout.NORTH);
+        contentPane.add(configPane, BorderLayout.SOUTH);
         contentPane.setBackground(Color.darkGray);
 
         frame.setBackground(Color.darkGray);
         frame.pack();
-        frame.setVisible(true);
         frame.setResizable(false);
+        frame.setVisible(true);
 
-        Action action = new AbstractAction() {
+        Action msgAction = new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String text = textField.getText();
-                textField.setText("");
+                String text = msgField.getText();
+                msgField.setText("");
                 if (text.contains("stop")) {
                     System.out.println("stopping client...");
                     System.exit(0);
@@ -92,7 +126,43 @@ public class Client implements Closeable {
                 sendToServer(text);
             }
         };
-        textField.addActionListener(action);
+        msgField.addActionListener(msgAction);
+        Action portAction = new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (portSelectField.getText().isEmpty()) return; // cancel if fields not present
+                port = Integer.parseInt(portSelectField.getText());
+                portSelectField.setText("");
+
+                portLabel.setText("Current port: " + port);
+            }
+        };
+        portSelectField.addActionListener(portAction);
+        Action hostAction = new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (hostField.getText().isEmpty()) return; // cancel if fields not present
+                hostname = hostField.getText();
+                hostField.setText("");
+
+                hostLabel.setText("Current host: " + hostname);
+            }
+        };
+        hostField.addActionListener(hostAction);
+        Action connectAction = new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (hostname == null || hostname.isEmpty() || port < 1 || connected) return;
+                try {
+                    myNetCon.startConnection(hostname,port);
+                    connectButton.setEnabled(false);
+                    addText("Connected to " + hostname + " on port " + port);
+                } catch (IOException ex) {
+                    JOptionPane.showMessageDialog(frame,ex.toString(),"Connect Failed",JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        };
+        connectButton.addActionListener(connectAction);
     }
 
     public static void addText(String txt) {
