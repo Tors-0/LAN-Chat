66738e8d007e33cf381d69ec59e562dbdea8a778
rae <raej0000@gmail.com>
2024-01-23 20:09:30 -0800

    custom notification icon for windows

--- src/io/github/Tors_0/client/SysTrayToast.java
+++ src/io/github/Tors_0/client/SysTrayToast.java
@@ -3,7 +3,7 @@ package io.github.Tors_0.client;
 import java.awt.*;
 
 public class SysTrayToast {
-    static Image icon = Toolkit.getDefaultToolkit().createImage(SysTrayToast.class.getResource("/io/github/Tors_0/client/resources/icon.png"));
+    static Image icon = Toolkit.getDefaultToolkit().createImage(SysTrayToast.class.getResource("/io/github/Tors_0/client/resources/lanchat.png"));
     static SystemTray tray = SystemTray.getSystemTray();
     public static void display(String message) throws AWTException {
         TrayIcon trayIcon = new TrayIcon(icon,"LAN-Chat");
