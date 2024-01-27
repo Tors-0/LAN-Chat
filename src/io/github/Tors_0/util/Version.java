089621c6ee0b4850b59af7a076f9f953b5da64fb
rae <raej0000@gmail.com>
2024-01-26 17:14:16 -0800

    resizable test 01

--- src/io/github/Tors_0/util/Version.java
+++ src/io/github/Tors_0/util/Version.java
@@ -1,16 +1,19 @@
 package io.github.Tors_0.util;
 
 import java.io.IOException;
+import java.util.jar.Attributes;
 import java.util.jar.Manifest;
 
 public class Version {
     private static String version;
+    private static String title;
     private static boolean versionPresent = false;
 
     static {
         try {
-            Manifest manifest = new Manifest(Version.class.getResourceAsStream("/io/github/Tors_0/META-INF/MANIFEST.MF"));
-            version = manifest.getMainAttributes().getValue("Implementation-Version");
+            Attributes manifest = new Manifest(Version.class.getResourceAsStream("/io/github/Tors_0/META-INF/MANIFEST.MF")).getMainAttributes();
+            version = manifest.getValue("Implementation-Version");
+            title = manifest.getValue("Implementation-Title");
             versionPresent = true;
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -18,6 +21,9 @@ public class Version {
     }
     public static String getVersion() {
         return version;
+    }
+    public static String getTitle() {
+        return title;
     }
     public static boolean isVersionPresent() {
         return versionPresent;
